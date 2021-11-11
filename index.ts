6cb971d3c94367ab42568efbcc88ac0cea01a4b3
lk-vila <lk-vila@protonmail.com>
2021-11-11 17:05:32 -0300

    Initial develpment of the backend
    Co-authored-by: João V. Soares <docputs@users.noreply.github.com>
    Co-authored-by: Raul Mello Silva <raulmel1o@users.noreply.github.com>
    Co-authored-by: daitamae <daitamae@users.noreply.github.com>
    Co-authored-by: Leonardo Meireles <LeonardoMeireles@users.noreply.github.com>
    Co-authored-by: Ygor Sad <ysads@users.noreply.github.com>

--- index.ts
+++ index.ts
@@ -1,8 +1,43 @@
+import axios from 'axios'
 import express from 'express'
+import fs from 'fs'
+import FormData from 'form-data'
+import multer from 'multer'
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 80
+const upload = multer({ storage: multer.memoryStorage()  })
 
 app.get('/', () => console.log("Drake best wrapper"))
+
+app.post('/predict', upload.single('audio_file'), async (req, res) => {
+  
+    const formData = new FormData()
+
+    formData.append("audio", new Blob([req.file.buffer]))
+    //formData.append("audio", req.body.file.buffer, req.body.file.originalname)
+    formData.append("sexo", req.body.sexo)
+    formData.append("idade", req.body.idade)
+    formData.append("nivel_falta_de_ar", req.body.nivel_falta_de_ar)
+
+    const spiraApiResponse = await axios.post("https://spira-api.herokuapp.com/predict", formData)
+    
+    res.status(200).json(`File uploaded`)
+})
+
+app.post('/predictv2', async (req, res) => {
+  
+
+    
+    formData.append("audio", new Blob(, ))
+    //formData.append("audio", req.body.file.buffer, req.body.file.originalname)
+    formData.append("sexo", req.body.sexo)
+    formData.append("idade", req.body.idade)
+    formData.append("nivel_falta_de_ar", req.body.nivel_falta_de_ar)
+
+    const spiraApiResponse = await axios.post("https://spira-api.herokuapp.com/predict", formData)
+    
+    res.status(200).json(`File uploaded`)
+})
 
 app.listen(port, () => console.log(`⚡️[server]: Server is running at https://localhost:${port}`))
